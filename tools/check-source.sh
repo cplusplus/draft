ce78eced98e3e4dfe99eb409398267e5f00126aa
Jens Maurer <Jens.Maurer@gmx.net>
2025-06-25 12:35:16 +0200

    [std,check] Ensure proper placement of \iref

--- tools/check-source.sh
+++ tools/check-source.sh
@@ -100,6 +100,14 @@ grep -n 'unicode{[^}]*[^0-9a-f}][^}]*}' $texfiles |
 grep -n '.(\\ref' $texfiles  | grep -v -- "--" |
     fail 'use \\iref instead of (\\ref' || failed=1
 
+# \iref cannot be at the start of a line
+grep -n '^\\iref' $texfiles |
+    fail '\\iref must be flush against the preceding word, not at the start of a line' || failed=1
+
+# \iref cannot be preceded by space
+grep -n ' \\iref' $texfiles |
+    fail '\\iref must not be preceded by space' || failed=1
+
 # Use \xrefc instead of "ISO C x.y.z"
 grep -n "^ISO C [0-9]*\." $texfiles |
     fail 'use \\xrefc instead' || failed=1
