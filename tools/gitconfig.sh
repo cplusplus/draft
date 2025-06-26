69acc8b5ecd61da803b05b7e961ad3301e0e733d
Matthias Kretz <m.kretz@gsi.de>
2025-06-26 18:33:29 +0200

    Use ${brace} syntax in shell script, as requested

--- tools/gitconfig.sh
+++ tools/gitconfig.sh
@@ -4,13 +4,13 @@ git config diff.c++draft.xfuncname '\\rSec[0-9]+(\[.*\])\{'
 
 precommit="$(git rev-parse --git-dir)/hooks/pre-commit"
 
-test -f "$precommit" && exit
+test -f "${precommit}" && exit
 read -p "Install 'make check' pre-commit hook? [Y/n] " hook
-if [ -z "$hook" -o "$hook" = "y" -o "$hook" = "Y" ]; then
-  cat <<EOF > "$precommit"
+if [ -z "${hook}" -o "${hook}" = "y" -o "${hook}" = "Y" ]; then
+  cat <<EOF > "${precommit}"
 #!/bin/sh
 cd \$(git rev-parse --show-toplevel)/source
 make check
 EOF
-  chmod +x "$precommit"
+  chmod +x "${precommit}"
 fi
