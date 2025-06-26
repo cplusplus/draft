47d9c366d2628849a42e12aade3c27c59034459e
Matthias Kretz <M.Kretz@gsi.de>
2025-06-26 18:49:38 +0200

    [Makefile, tools] Add "check" target and provide pre-commit hook for 'make check' (#8021)
    As a drive-by fix, add all other phony targets to .PHONY.

--- tools/gitconfig.sh
+++ tools/gitconfig.sh
@@ -1,3 +1,16 @@
 #!/bin/sh
 git config diff.orderFile .gitorder
 git config diff.c++draft.xfuncname '\\rSec[0-9]+(\[.*\])\{'
+
+precommit="$(git rev-parse --git-dir)/hooks/pre-commit"
+
+test -f "${precommit}" && exit
+read -p "Install 'make check' pre-commit hook? [Y/n] " hook
+if [ -z "${hook}" -o "${hook}" = "y" -o "${hook}" = "Y" ]; then
+  cat <<EOF > "${precommit}"
+#!/bin/sh
+cd \$(git rev-parse --show-toplevel)/source
+make check
+EOF
+  chmod +x "${precommit}"
+fi
